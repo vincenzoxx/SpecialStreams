c59525164906f648f754919dea42f1f9e70c2410
vincenzoxx <50289647+vincenzoxx@users.noreply.github.com>
2020-08-13 09:58:37 +0200

    Create IGPSCollection.cs

---
+++ IGPSCollection.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace G20CustomControls
+{
+	public interface IGPSCollection : IList<IGPS>, IMediaViewerItem, INotifyCollectionChanged
+	{
+
+	}
+}
